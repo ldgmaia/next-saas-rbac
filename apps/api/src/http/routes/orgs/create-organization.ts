24f0b9c2b6a10d3bf8222e1de3517ef92546b17a
Diego Maia <ldgmaia@gmail.com>
2024-05-10 12:07:12 -0400

    Feat: organization creation complete

---
+++ apps/api/src/http/routes/orgs/create-organization.ts
@@ -0,0 +1,78 @@
+import { auth } from '@/http/middlewares/auth'
+import { prisma } from '@/lib/prisma'
+import { createSlug } from '@/utils/create-slug'
+import type { FastifyInstance } from 'fastify'
+import type { ZodTypeProvider } from 'fastify-type-provider-zod'
+import z from 'zod'
+import { BadRequestError } from '../_errors/bad-request-error'
+
+export async function createOrganization(app: FastifyInstance) {
+  app
+    .withTypeProvider<ZodTypeProvider>()
+    .register(auth)
+    .post(
+      '/organizations',
+      {
+        schema: {
+          tags: ['Organizations'],
+          summary: 'Create an organization',
+          security: [{ apiKey: [] }],
+          body: z.object({
+            name: z.string(),
+            domain: z.string().nullish(),
+            shouldAttachUserByDomain: z.boolean().optional(),
+          }),
+          response: {
+            201: z.object({
+              organizationId: z.string().uuid(),
+            }),
+            401: z.object({
+              error: z.string().uuid(),
+            }),
+          },
+        },
+      },
+      async (request, reply) => {
+        const userId = await request.getCurrentUserId()
+        const { name, domain, shouldAttachUserByDomain } = request.body
+
+        if (domain) {
+          const organizationByDomain = await prisma.organization.findUnique({
+            where: { domain },
+          })
+
+          if (organizationByDomain) {
+            throw new BadRequestError(
+              'Another organization is using this domain'
+            )
+          }
+        }
+
+        // const organizationNameExists = await prisma.organization.findFirst({
+        //   where: { name },
+        // })
+
+        // if (organizationNameExists) {
+        //   throw new BadRequestError('Another organization is using this name')
+        // }
+
+        const organization = await prisma.organization.create({
+          data: {
+            name,
+            slug: createSlug(name),
+            domain,
+            shouldAttachUserByDomain,
+            ownerId: userId,
+            members: {
+              create: {
+                userId,
+                role: 'ADMIN',
+              },
+            },
+          },
+        })
+
+        return reply.status(201).send({ organizationId: organization.id })
+      }
+    )
+}
