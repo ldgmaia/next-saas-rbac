d0b426299f4317be7f7b21476b54dee1852139ba
Diego Maia <ldgmaia@gmail.com>
2024-05-14 10:21:14 -0400

    Feat: get organization info

---
+++ apps/api/src/http/routes/orgs/get-organizations.ts
@@ -0,0 +1,72 @@
+import { auth } from '@/http/middlewares/auth'
+import { prisma } from '@/lib/prisma'
+import { roleSchema } from '@saas/auth'
+import type { FastifyInstance } from 'fastify'
+import type { ZodTypeProvider } from 'fastify-type-provider-zod'
+import { z } from 'zod'
+
+export async function getOrganizations(app: FastifyInstance) {
+  app
+    .withTypeProvider<ZodTypeProvider>()
+    .register(auth)
+    .get(
+      '/organizations',
+      {
+        schema: {
+          tags: ['Organizations'],
+          summary: 'Get organizations where user is member',
+          security: [{ apiKey: [] }],
+          response: {
+            200: z.object({
+              organizations: z.array(
+                z.object({
+                  id: z.string().uuid(),
+                  name: z.string(),
+                  slug: z.string(),
+                  avatarUrl: z.string().nullable(),
+                  role: roleSchema,
+                })
+              ),
+            }),
+          },
+        },
+      },
+      async (request, reply) => {
+        const userId = await request.getCurrentUserId()
+
+        const organizations = await prisma.organization.findMany({
+          select: {
+            id: true,
+            name: true,
+            slug: true,
+            avatarUrl: true,
+            members: {
+              select: {
+                role: true,
+              },
+            },
+          },
+          where: {
+            members: {
+              some: {
+                userId,
+              },
+            },
+          },
+        })
+
+        const organizationsWithUserRole = organizations.map(
+          ({ members, ...org }) => {
+            return {
+              ...org,
+              role: members[0].role,
+            }
+          }
+        )
+
+        return {
+          organizations: organizationsWithUserRole,
+        }
+      }
+    )
+}
