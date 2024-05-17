bf120a958a266ed9fbed7f2f8e6b7340371b8af0
Diego Maia <ldgmaia@gmail.com>
2024-05-17 15:22:26 -0400

    Feat: Delete project complete, with permission check

--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -27,6 +27,7 @@ import { shutdownOrganization } from './routes/orgs/shutdown-organization'
 import { transferOrganization } from './routes/orgs/transfer-organization'
 import { updateOrganization } from './routes/orgs/update-organization'
 import { createProject } from './routes/projects/create-project'
+import type { deleteProject } from './routes/projects/delete-project'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
@@ -80,6 +81,7 @@ app.register(shutdownOrganization)
 app.register(transferOrganization)
 
 app.register(createProject)
+app.register(deleteProject)
 
 app.listen({ port: env.SERVER_PORT }).then(() => {
   console.log('Running on port 3333')
