470d5c878bb7ed1a4f4f10f5fd620b43cac3acff
Diego Maia <ldgmaia@gmail.com>
2024-05-23 09:09:42 -0400

    Feat: update member details

---
+++ apps/api/src/http/routes/members/update-member.ts
@@ -0,0 +1,72 @@
+import { auth } from '@/http/middlewares/auth'
+import { prisma } from '@/lib/prisma'
+import { getUserPermissions } from '@/utils/get-user-permissions'
+import { roleSchema } from '@saas/auth'
+import type { FastifyInstance } from 'fastify'
+import type { ZodTypeProvider } from 'fastify-type-provider-zod'
+import { z } from 'zod'
+import { UnauthorizedError } from '../_errors/unauthorized-error'
+
+// id: string;
+// role: $Enums.Role;
+// name: string | null;
+// avatarUrl: string | null;
+// email: string;
+
+export async function updateMembers(app: FastifyInstance) {
+  app
+    .withTypeProvider<ZodTypeProvider>()
+    .register(auth)
+    .put(
+      '/organizations/:slug/members/:memberId',
+      {
+        schema: {
+          tags: ['Members'],
+          summary: 'Update member details',
+          security: [{ apiKey: [] }],
+          params: z.object({
+            slug: z.string(),
+            memberId: z.string().uuid(),
+          }),
+          body: z.object({
+            role: roleSchema,
+          }),
+          response: {
+            204: z.null(),
+          },
+        },
+      },
+      async (request, reply) => {
+        const { slug, memberId } = request.params
+        const userId = await request.getCurrentUserId()
+        const { organization, membership } =
+          await request.getUserMembership(slug)
+
+        const { cannot } = getUserPermissions(userId, membership.role)
+
+        if (cannot('update', 'User')) {
+          throw new UnauthorizedError(
+            'You are not allowed to update this member'
+          )
+        }
+
+        const { role } = request.body
+
+        console.log('role ', role)
+        console.log('memberId ', memberId)
+        console.log('organization.id ', organization.id)
+
+        await prisma.member.update({
+          where: {
+            id: memberId,
+            organizationId: organization.id,
+          },
+          data: {
+            role,
+          },
+        })
+
+        return reply.status(204).send()
+      }
+    )
+}
--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -19,7 +19,8 @@ import { createAccount } from './routes/auth/create-account'
 import { getProfile } from './routes/auth/get-profile'
 import { requestPasswordRecover } from './routes/auth/request-password-recover'
 import { resetPassword } from './routes/auth/reset-password'
-import { getMembers } from './routes/members/get-members'
+import { getMembers } from './routes/members/get-member'
+import { updateMembers } from './routes/members/update-member'
 import { createOrganization } from './routes/orgs/create-organization'
 import { getMembership } from './routes/orgs/get-membership'
 import { getOrganization } from './routes/orgs/get-organization'
@@ -91,6 +92,7 @@ app.register(getProjects)
 app.register(updateProject)
 
 app.register(getMembers)
+app.register(updateMembers)
 
 app.listen({ port: env.SERVER_PORT }).then(() => {
   console.log('Running on port 3333')
