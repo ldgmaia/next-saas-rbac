1834ab43a17738b9bf14c0456656f0aefe6b6e54
Diego Maia <ldgmaia@gmail.com>
2024-06-12 11:08:22 -0400

    Some Eslint/Prettier fix and login page layout

--- config/eslint-config/next.js
+++ config/eslint-config/next.js
@@ -1,9 +1,17 @@
 /** @type {import('eslint').Linter.Config} */
 
 module.exports = {
-  extends: ['@rocketseat/eslint-config/next'],
+  extends: ['@rocketseat/eslint-config/next', 'plugin:react/recommended'],
   plugins: ['simple-import-sort'],
   rules: {
+    'react/react-in-jsx-scope': 'off',
+    'react/self-closing-comp': [
+      'error',
+      {
+        component: true,
+        html: true,
+      },
+    ],
     'simple-import-sort/imports': 'error',
     'prettier/prettier': [
       'error',
