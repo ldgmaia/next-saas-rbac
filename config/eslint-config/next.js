e449d3b9874351306a02c93a392dd9415246b8c6
Diego Maia <ldgmaia@gmail.com>
2024-08-07 15:27:07 -0400

    Feat: update member role

--- config/eslint-config/next.js
+++ config/eslint-config/next.js
@@ -14,6 +14,7 @@ module.exports = {
           'decorative',
           'sideOffset',
           'checked',
+          'position',
         ],
       },
     ], // needed because of shadcn ui empty className
