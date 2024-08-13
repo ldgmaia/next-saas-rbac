f4220a32fb6b59baead4ae5eb6716ecce1fb614e
Diego Maia <ldgmaia@gmail.com>
2024-08-13 15:52:21 -0400

    Feat: project list done

--- apps/web/src/app/(app)/org/[slug]/(projects)/project-list.tsx
+++ apps/web/src/app/(app)/org/[slug]/(projects)/project-list.tsx
@@ -1,5 +1,8 @@
+import dayjs from 'dayjs'
+import relativeTime from 'dayjs/plugin/relativeTime'
 import { ArrowRight } from 'lucide-react'
 
+import { getCurrentOrganization } from '@/auth/auth'
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
 import { Button } from '@/components/ui/button'
 import {
@@ -9,37 +12,47 @@ import {
   CardHeader,
   CardTitle,
 } from '@/components/ui/card'
+import { getProjects } from '@/http/get-projects'
 
-export function ProjectList() {
+dayjs.extend(relativeTime)
+
+export async function ProjectList() {
+  const currentOrg = getCurrentOrganization()
+  const { projects } = await getProjects(currentOrg!)
+
   return (
     <div className="grid grid-cols-3 gap-4">
-      <Card>
-        <CardHeader>
-          <CardTitle>Project 01</CardTitle>
+      {projects.map((project) => (
+        <Card key={project.id} className="flex flex-col justify-between">
+          <CardHeader>
+            <CardTitle className="text-xl font-medium">
+              {project.name}
+            </CardTitle>
 
-          <CardDescription className="line-clamp-2 leading-relaxed">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vel ipsa
-            unde maxime facilis commodi soluta totam dolores quae voluptatem
-            vitae quisquam odit earum officia veniam, omnis aut optio provident
-            eaque?
-          </CardDescription>
-        </CardHeader>
-        <CardFooter className="flex items-center gap-1.5">
-          <Avatar className="size-4">
-            <AvatarImage src="https://github.com/ldgmaia.png" />
-            <AvatarFallback />
-          </Avatar>
-          <span className="text-xs text-muted-foreground">
-            Created by{' '}
-            <span className="font-medium text-foreground">Diego Maia</span> a
-            day ago
-          </span>
+            <CardDescription className="line-clamp-2 leading-relaxed">
+              {project.description}
+            </CardDescription>
+          </CardHeader>
+          <CardFooter className="flex items-center gap-1.5 ">
+            <Avatar className="size-4">
+              {project.owner.avatarUrl && (
+                <AvatarImage src={project.owner.avatarUrl} />
+              )}
+              <AvatarFallback />
+            </Avatar>
+            <span className="truncate text-xs text-muted-foreground">
+              <span className="font-medium text-foreground">
+                {project.owner.name}
+              </span>{' '}
+              {dayjs(project.createdAt).fromNow()}
+            </span>
 
-          <Button size="xs" variant="outline" className="ml-auto">
-            View <ArrowRight className="ml-2 size-3" />
-          </Button>
-        </CardFooter>
-      </Card>
+            <Button size="xs" variant="outline" className="ml-auto">
+              View <ArrowRight className="ml-2 size-3" />
+            </Button>
+          </CardFooter>
+        </Card>
+      ))}
     </div>
   )
 }
