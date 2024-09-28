5e30bde9bf96315a4f8333ee24307334e963b6eb
Diego Maia <ldgmaia@gmail.com>
2024-09-27 20:04:37 -0400

    Chores: database setup

--- apps/api/prisma/migrations/20240918023340_initial/migration.sql
+++ apps/api/prisma/migrations/20240918023340_initial/migration.sql
@@ -81,7 +81,7 @@ CREATE TABLE `projects` (
     `id` VARCHAR(191) NOT NULL,
     `name` VARCHAR(191) NOT NULL,
     `slug` VARCHAR(191) NOT NULL,
-    `description` LONGTEXT NOT NULL,
+    `description` VARCHAR(500) NOT NULL,
     `avatar_url` VARCHAR(191) NULL,
     `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
     `updated_at` DATETIME(3) NOT NULL,
